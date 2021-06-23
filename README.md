68837e7e563332b717adfe195fc280fa9c49856a
Oliver-makes-code <59418903+Oliver-makes-code@users.noreply.github.com>
2021-06-23 14:31:26 -0500

    Coded stuff.

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Fabric Example Mod
-
-## Setup
-
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
-
-## License
-
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+#Simple Right Click Harvest
+##Does what it says. It's a right click harvest mod.
+###Ask for a forge port and I will eat your keyboard
---
+++ src/main/java/com/github/olivermakescode/srch/SimpleHarvest.java
@@ -0,0 +1,37 @@
+package com.github.olivermakescode.srch;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.BeetrootsBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropBlock;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+
+public class SimpleHarvest implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			BlockState blockState = world.getBlockState(hitResult.getBlockPos());
+			BlockPos pos = hitResult.getBlockPos();
+			if (blockState.getBlock() instanceof BeetrootsBlock) {
+				int age = blockState.get(BeetrootsBlock.AGE);
+				if (age == 3) {
+					world.breakBlock(pos,true,player);
+					world.setBlockState(pos,blockState.with(BeetrootsBlock.AGE,0));
+					return ActionResult.SUCCESS;
+				}
+			}
+			if (blockState.getBlock() instanceof CropBlock) {
+				int age = blockState.get(CropBlock.AGE);
+				if (age == 7) {
+					world.breakBlock(pos,true,player);
+					world.setBlockState(pos,blockState.with(CropBlock.AGE,0));
+					return ActionResult.SUCCESS;
+				}
+			}
+			return ActionResult.PASS;
+		});
+	}
+}
